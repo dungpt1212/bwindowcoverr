38eea52c41d1fdc13adf13ef2958c03996fbad7a
thanh.pham <pnthanh80@hotmail.com>
2021-06-02 21:02:50 +1000

    1. Added MDB bootstrap 2. Playing around with the carousel on the home page.

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,10 @@
 </template>
 
 <style lang="scss">
+@import '~@/../mdb/scss/index.free.scss';
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
